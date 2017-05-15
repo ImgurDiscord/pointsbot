d56677864136ef530b20d32c1dd6bd98d82a6194
tabloida <aydanartist@gmail.com>
2017-05-15 17:07:48 -0400

    fixed .profile

--- server/index.js
+++ server/index.js
@@ -1002,7 +1002,7 @@ bot.on("message", msg => {
 		
 		var download = function(uri, filename, callback){
 			request.head(uri, function(err, res, body){
-				/*console.log('content-type:', res.headers['content-type']);
+				console.log('content-type:', res.headers['content-type']);
 				console.log('content-length:', res.headers['content-length']);
 
 				request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
