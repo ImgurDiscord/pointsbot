633ccd8d00d6dbe8c9ae62697b8c250a37fa4250
tabloida <aydanartist@gmail.com>
2017-05-18 23:12:50 -0400

    fix?...nah

--- server/index.js
+++ server/index.js
@@ -1218,11 +1218,12 @@ bot.on("message", msg => {
 			});
 
 			stream.on('end', function(){
+				console.log("Image saved to file.");
 				stream.end();
+				msg.channel.send("Place your bets!\n>Chicken 1 *" + conditions1 + "*.\n>Chicken 2 *" + conditions2 + "*.\nBet on a chicken with `.bet <1 or 2>`", {files: ["server/cockfight.png"]});
 			});
 		
-			
-			msg.channel.send("Place your bets!\n>Chicken 1 *" + conditions1 + "*.\n>Chicken 2 *" + conditions2 + "*.\nBet on a chicken with `.bet <1 or 2>`", {files: ["server/cockfight.png"]});
+			//msg.channel.send("Place your bets!\n>Chicken 1 *" + conditions1 + "*.\n>Chicken 2 *" + conditions2 + "*.\nBet on a chicken with `.bet <1 or 2>`", {files: ["server/cockfight.png"]});
 		
 		const collector = msg.channel.createCollector(
 			m => m.content.startsWith(".bet"),
