d1c4b7293f563e502d3368e26443f0ebf9d9726b
tabloida <aydanartist@gmail.com>
2017-07-22 15:36:38 -0400

    added god preventer 2000

--- server/index.js
+++ server/index.js
@@ -80,9 +80,6 @@ bot.on("ready", () => {
 bot.on("message", message => {
 	var godId = "192711002117242880";
 	
-	if (message.author.id == godId) {
-		message.author.send("You are unable to do that at this time, sorry!");
-	}
 	
 	servername = message.guild.name;
 	if (servername.includes('Meet hot single')) {
